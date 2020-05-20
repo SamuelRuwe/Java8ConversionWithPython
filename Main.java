85e9b6f756e9dbf8bca0f4924ac023b6a2048433
Sam <ruwesam@gmail.com>
2020-05-20 15:41:26 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.text.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+//find a way to regex appropriately for .* extension
---
+++ convert.py
@@ -0,0 +1,23 @@
+with open('Main.java', 'r') as rf:
+    with open('test_copy.java', 'w') as wf:
+        with open('Java8to11Dictionary.txt', 'r') as dictFile:
+            dict = {}
+            for line in dictFile:
+                (key, val) = line.split()
+                dict[str(key)] = val
+            # print(dict)
+            for line in rf:
+                for key in dict:
+                    testString = key
+                    a = line.lower()
+                    pos = a.find(testString)
+                    if pos != -1:
+                        newString = dict[key]
+                        b = line[0:pos]
+                        c = line[(pos+len(testString)):-1]
+                        wf.write(str(b) + str(newString) + str(c) + str('\n'))
+                        break
+                    elif dict[key] == "eof":
+                        wf.write(line)
+
+#optimize this code by checking package names in alphabetical order
